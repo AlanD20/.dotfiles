4b683cbecd4a8833cdcbca247ec0d34befdc40d0
AlanD20 <aland20@pm.me>
2023-08-02 15:22:07 +0200

    Add docs to binaries

--- bin/.local/bin/sway-inactive-transparency.py
+++ bin/.local/bin/sway-inactive-transparency.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 import i3ipc
 
-transparency_val = '0.85'
+#
+# Usage:
+#
+# Run this binary when you startup your sway so that it always listen
+#
+
+transparency_val = "0.85"
 ipc = i3ipc.Connection()
 prev_focused = None
 
@@ -9,14 +15,16 @@ for window in ipc.get_tree():
     if window.focused:
         prev_focused = window
     else:
-        window.command('opacity ' + transparency_val)
+        window.command("opacity " + transparency_val)
 
 
 def on_window_focus(ipc, focused):
     global prev_focused
-    if focused.container.id != prev_focused.id:  # https://github.com/swaywm/sway/issues/2859
-        focused.container.command('opacity 1')
-        prev_focused.command('opacity ' + transparency_val)
+    if (
+        focused.container.id != prev_focused.id
+    ):  # https://github.com/swaywm/sway/issues/2859
+        focused.container.command("opacity 1")
+        prev_focused.command("opacity " + transparency_val)
         prev_focused = focused.container
 
 
