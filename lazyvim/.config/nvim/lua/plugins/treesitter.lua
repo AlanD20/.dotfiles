9a9e47262a3c4384d8de9c0a4961db51eae6a305
AlanD20 <aland20@pm.me>
2023-08-07 22:59:43 +0200

    Add treesitter playground
    - Fix ensure installed treesitter options

--- lazyvim/.config/nvim/lua/plugins/treesitter.lua
+++ lazyvim/.config/nvim/lua/plugins/treesitter.lua
@@ -45,6 +45,35 @@ return {
         "yaml",
         "zig",
       })
+
+      opts.playground = vim.tbl_deep_extend("force", {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        query_linter = {
+          enable = true,
+          use_virtual_text = true,
+          lint_events = { "BufWrite", "CursorHold" },
+        },
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      }, opts.playground or {})
+
+      return opts
     end,
+  },
+  {
+    "nvim-treesitter/playground",
   },
 }
