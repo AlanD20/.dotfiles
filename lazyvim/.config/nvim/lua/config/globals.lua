2043f7d32d0fcec350cd9ad4dcd452df026f7c79
AlanD20 <aland20@pm.me>
2023-08-16 10:47:33 +0200

    nvim: Add new glob fun

--- lazyvim/.config/nvim/lua/config/globals.lua
+++ lazyvim/.config/nvim/lua/config/globals.lua
@@ -9,25 +9,37 @@ P = function(...)
 end
 
 -- Return if given item exists, otherwise, return fallback.
--- @param item string: The item to search for or expand.
--- @param fallback string: The fallback value if the item is not found.
+-- @param item string: The flie to search for or expand.
+-- @param fallback string: The fallback value if the file is not found.
 -- @param opts table (optional): Options for the search, passed to `vim.fs.find` options
 -- @return string: The target path or fallback value.
-FindOrFallback = function(item, fallback, opts)
+FindOrFallback = function(file, fallback, opts)
   opts = opts or {}
 
-  item = vim.fn.expand(item)
+  file = vim.fn.expand(file)
 
   local target = ""
 
-  local files = vim.fs.find(item, opts)
+  local files = vim.fs.find(file, opts)
 
   if files[1] ~= nil then
-    -- if path exists, concat it, otherwise, return item alone
-    target = opts["path"] and opts.path .. "/" .. item or item
+    -- if path exists, concat it, otherwise, return file alone
+    target = opts["path"] and opts.path .. "/" .. file or file
   else
     target = vim.fn.expand(fallback)
   end
 
   return target
+end
+
+-- Return true if file exists, otherwise false
+-- @param item string: The file to search for or expand.
+-- @param opts table (optional): Options for the search, passed to `vim.fs.find` options
+-- @return boolean
+FileExists = function(file, opts)
+  opts = opts or {}
+
+  local files = vim.fs.find(file, opts)
+
+  return files[1] ~= nil
 end
