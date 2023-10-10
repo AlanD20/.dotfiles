2e37b74f1444fb4f9bf9a739e8186e7c857445b6
AlanD20 <aland20@pm.me>
2023-10-10 10:10:05 +0200

    Add ufo.nvim to fold using treesitter
    - Bump nvim plugin versions

---
+++ lazyvim/.config/nvim/lua/plugins/ufo.lua
@@ -0,0 +1,16 @@
+return {
+  -- Folding capabilities
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+    config = function()
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end,
+      })
+    end,
+  },
+}
