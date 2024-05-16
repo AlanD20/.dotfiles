38de9b500335708cf8760a0dc50457f500905bb6
AlanD20 <aland20@pm.me>
2024-05-16 20:16:07 +0200

    bump nvim packages

--- lazyvim/.config/nvim/lua/config/lazy.lua
+++ lazyvim/.config/nvim/lua/config/lazy.lua
@@ -1,3 +1,4 @@
+-- the function returns path to `$HOME/.local/share/nvim`, then concatenates `lazy/lazy.nvim`
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
@@ -5,6 +6,9 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
     lazypath })
 end
+
+-- Add lazy to the `runtimepath`, this allows us to `require` it.
+-- learn more at `:help runtimepath`.
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
