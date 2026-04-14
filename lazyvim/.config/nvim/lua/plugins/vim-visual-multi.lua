7f85dba1f9140cd462c8e930042fb6dfc2b922c4
AlanD20 <aland20@pm.me>
2026-04-14 13:21:45 +0200

    fix vim-visual-multi plugin

--- lazyvim/.config/nvim/lua/plugins/vim-visual-multi.lua
+++ lazyvim/.config/nvim/lua/plugins/vim-visual-multi.lua
@@ -4,8 +4,19 @@ return {
     branch = "master",
     init = function()
       vim.cmd("let g:VM_maps = {}")
+      vim.cmd("let g:VM_maps['Find Under'] = '<C-n>'")
+      vim.cmd("let g:VM_maps['Find Subword Under'] = '<C-n>'")
       vim.cmd("let g:VM_maps['Add Cursor Down'] = '<M-Down>'")
       vim.cmd("let g:VM_maps['Add Cursor Up'] = '<M-Up>'")
+      vim.cmd("let g:VM_maps['Find Next'] = 'n'")
+      vim.cmd("let g:VM_maps['Find Prev'] = 'N'")
+      vim.cmd("let g:VM_maps['Skip Region'] = 'q'")
+      vim.cmd("let g:VM_maps['Remove Region'] = 'Q'")
     end,
+    keys = {
+      { "<C-n>", mode = { "n", "v" }, desc = "Find Under (Multi-cursor)" },
+      { "<M-Down>", mode = { "n", "v" }, desc = "Add Cursor Down" },
+      { "<M-Up>", mode = { "n", "v" }, desc = "Add Cursor Up" },
+    },
   },
 }
